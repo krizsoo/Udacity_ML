61f39346a4a11ad664d0ae1e7a6c8e706fb5607a
krizsoo <krizsoo@gmail.com>
2018-06-09 23:14:01 +0200

    added AdaBoost classifier to test

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -30,12 +30,16 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
-from sklearn.neighbors import NearestNeighbors
-clf = NearestNeighbors()
-clf.fit(features_train)
+#from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
+#clf = KNeighborsClassifier(n_neighbors=20, algorithm='auto', weights='distance')
+clf = AdaBoostClassifier()
+clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
 
-
+print(accuracy_score(pred, labels_test))
 
 try:
     prettyPicture(clf, features_test, labels_test)
