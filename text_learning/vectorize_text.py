4eaa3f98c3b4fc334495db6c2dafe1c0149e93f4
krizsoo <krizsoo@gmail.com>
2018-07-31 07:39:26 +0200

    text learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -48,10 +48,13 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            str = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            replace_words = ["sara", "shackleton", "chris", "germani"]
+            for i in replace_words:
+                str = str.replace(i, '')
+            print(str)
             ### append the text to word_data
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
@@ -71,5 +74,3 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
-
-
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -8,15 +8,16 @@ def parseOutText(f):
         metadata block at the top
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
-        in the email (space-separated) 
-        
+        in the email (space-separated)
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
+
         """
 
-
+    from nltk.stem.snowball import SnowballStemmer
+    stemmer = SnowballStemmer("english")
     f.seek(0)  ### go back to beginning of file (annoying)
     all_text = f.read()
 
@@ -28,19 +29,25 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
 
+    words = text_string.split()
+    output_words = []
+    for i in words:
+        output_words.append(stemmer.stem(i))
+
+    words = " ".join(output_words)
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+
 
 
 
 
     return words
 
-    
+
 
 def main():
     ff = open("../text_learning/test_email.txt", "r")
@@ -51,4 +58,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
