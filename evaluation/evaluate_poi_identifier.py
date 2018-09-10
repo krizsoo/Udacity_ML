f2f42c0acd35e361b23677c7929a93bcb827a7c0
krizsoo <krizsoo@gmail.com>
2018-09-10 21:31:28 +0200

    removing outliers

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -15,6 +15,9 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score
+
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -34,3 +37,20 @@ from sklearn.tree import DecisionTreeClassifier
 clf = DecisionTreeClassifier(random_state=0)
 clf.fit(features_train,labels_train)
 print clf.score(features_test,labels_test)
+prediction = clf.predict(features_test)
+
+counter = 0
+for i in prediction:
+    counter += i
+
+print counter
+
+true_positives = 0
+for i in range(len(prediction)):
+    if prediction[i] == labels_test[i] and prediction[i] == 1:
+        true_positives += 1
+
+print true_positives
+
+print precision_score(prediction, labels_test)
+print recall_score(prediction, labels_test)
--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -2,28 +2,55 @@
 
 import sys
 import pickle
+import matplotlib.pyplot as plt
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
+
+
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi', 'bonus', 'salary'] #total_payments', 'deferral_payments', 'loan_advances', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees', 'to_messages', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi'] # You will need to use more features
+
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+data_dict.pop('TOTAL', 0)
+
+### remove NAN's from dataset
+outliers = []
+for key in data_dict:
+    # print data_dict[key].values()[1]
+    for k,v in data_dict[key].items():
+        if v == 'NaN':
+              data_dict[key][k] = 0
+
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
-labels, features = targetFeatureSplit(data)
+
+from sklearn import preprocessing
+scaler = preprocessing.RobustScaler()
+data_scaled = scaler.fit_transform(data)
+labels, features = targetFeatureSplit(data_scaled)
+# for point in data:
+#     salary = point[4]
+#     bonus = point[2]
+#     matplotlib.pyplot.scatter( salary, bonus )
+#
+# matplotlib.pyplot.xlabel("salary")
+# matplotlib.pyplot.ylabel("bonus")
+# matplotlib.pyplot.show()
+
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -33,19 +60,34 @@ labels, features = targetFeatureSplit(data)
 
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+# clf = DecisionTreeClassifier()
+# clf = SVC(kernel='rbf', C=10000)
 clf = GaussianNB()
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+from sklearn.feature_selection import SelectPercentile
+slc = SelectPercentile(percentile=10)
+hurka = slc.fit_transform(features, labels)
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
+### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
 from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+    train_test_split(hurka, labels, test_size=0.3, random_state=42)
+
+
+clf.fit(features_train, labels_train)
+prediction = clf.predict(features_test)
+print prediction
+print clf.score(features_test, labels_test)
+
+
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
